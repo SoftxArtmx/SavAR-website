945afd900c28ab66ffbebc9e0d4ce527e6b05412
Diego Muñoz Rede <dmrede21@outlook.com>
2024-10-30 11:01:06 -0600

    Update: Added Footer lol.

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import 'normalize.css'
 import Home from './Home/Home.jsx'
+import Footer from './Footer/Footer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Home />
+    <Footer/>
   </StrictMode>,
 )
