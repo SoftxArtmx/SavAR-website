31a5de8e55da46de35649f2432b40305398922b8
Diego Muñoz Rede <dmrede21@outlook.com>
2024-10-31 23:23:35 -0600

    Update: Contact Added

---
+++ src/HomeContact/Contact/Contact.jsx
@@ -0,0 +1,297 @@
+import React from "react";
+import "./Contact.css"; // Incluye el archivo CSS
+
+function Contact () {
+    return (
+        <form id="contact-stack-form" class="form">
+			<div class="cube-1 w-100 cube">
+				<div class="front"></div>
+				<div class="back"></div>
+				<div class="top"></div>
+				<div class="bottom"></div>
+				<div class="left"></div>
+				<div class="right"></div>
+			</div>
+			<div class="cube-2 w-100 cube">
+				<div class="front">
+					<label for="name" class="label">Nombre</label>
+				</div>
+				<div class="back"></div>
+				<div class="top"></div>
+				<div class="bottom"></div>
+				<div class="left"></div>
+				<div class="right"></div>
+			</div>
+			<div class="cube-3 w-100 cube">
+				<div class="front"></div>
+				<div class="back"></div>
+				<div class="top"></div>
+				<div class="bottom"></div>
+				<div class="left"></div>
+				<div class="right"></div>
+			</div>
+			<div class="cube-4 w-300 cube">
+				<div class="front"></div>
+				<div class="back"></div>
+				<div class="top"></div>
+				<div class="bottom"></div>
+				<div class="left"></div>
+				<div class="right"></div>
+			</div>
+			<div class="cube-5 w-300 cube">
+				<div class="front">
+					<input type="text" name="name" id="name" placeholder="Tu nombre" class="field" />
+				</div>
+				<div class="back"></div>
+				<div class="top"></div>
+				<div class="bottom"></div>
+				<div class="left"></div>
+				<div class="right"></div>
+			</div>
+			<div class="cube-6 w-180 cube">
+				<div class="front"></div>
+				<div class="back"></div>
+				<div class="top"></div>
+				<div class="bottom"></div>
+				<div class="left"></div>
+				<div class="right"></div>
+			</div>
+			<div class="cube-7 w-120 cube">
+				<div class="front"></div>
+				<div class="back"></div>
+				<div class="top"></div>
+				<div class="bottom"></div>
+				<div class="left"></div>
+				<div class="right"></div>
+			</div>
+			<div class="cube-8 w-100 cube">
+				<div class="front"></div>
+				<div class="back"></div>
+				<div class="top"></div>
+				<div class="bottom"></div>
+				<div class="left"></div>
+				<div class="right"></div>
+			</div>
+			<div class="cube-9 w-300 cube">
+				<div class="front"></div>
+				<div class="back"></div>
+				<div class="top"></div>
+				<div class="bottom"></div>
+				<div class="left"></div>
+				<div class="right"></div>
+			</div>
+			<div class="cube-10 w-100 cube">
+				<div class="front">
+					<label for="email" class="label">Email</label>
+				</div>
+				<div class="back"></div>
+				<div class="top"></div>
+				<div class="bottom"></div>
+				<div class="left"></div>
+				<div class="right"></div>
+			</div>
+			<div class="cube-11 w-300 cube">
+				<div class="front"></div>
+				<div class="back"></div>
+				<div class="top"></div>
+				<div class="bottom"></div>
+				<div class="left"></div>
+				<div class="right"></div>
+			</div>
+			<div class="cube-12 w-300 cube">
+				<div class="front">
+					<input type="email" name="email" id="email" placeholder="Tu email" class="field" />
+				</div>
+				<div class="back"></div>
+				<div class="top"></div>
+				<div class="bottom"></div>
+				<div class="left"></div>
+				<div class="right">
+				</div>
+			</div>
+			<div class="cube-13 w-100 cube">
+				<div class="front"></div>
+				<div class="back"></div>
+				<div class="top"></div>
+				<div class="bottom"></div>
+				<div class="left"></div>
+				<div class="right"></div>
+			</div>
+			<div class="cube-14 w-100 cube">
+				<div class="front"></div>
+				<div class="back"></div>
+				<div class="top"></div>
+				<div class="bottom"></div>
+				<div class="left"></div>
+				<div class="right"></div>
+			</div>
+			<div class="cube-15 w-100 cube">
+				<div class="front"></div>
+				<div class="back"></div>
+				<div class="top"></div>
+				<div class="bottom"></div>
+				<div class="left"></div>
+				<div class="right">
+				</div>
+			</div>
+			<div class="cube-16 w-300 cube">
+				<div class="front"></div>
+				<div class="back"></div>
+				<div class="top"></div>
+				<div class="bottom"></div>
+				<div class="left"></div>
+				<div class="right"></div>
+			</div>
+			<div class="cube-17 w-300 cube">
+				<div class="front"></div>
+				<div class="back"></div>
+				<div class="top"></div>
+				<div class="bottom"></div>
+				<div class="left"></div>
+				<div class="right"></div>
+			</div>
+			<div class="cube-18 w-300 cube">
+				<div class="front"></div>
+				<div class="back"></div>
+				<div class="top"></div>
+				<div class="bottom"></div>
+				<div class="left"></div>
+				<div class="right"></div>
+			</div>
+			<div class="cube-19 w-100 cube">
+				<div class="front"></div>
+				<div class="back"></div>
+				<div class="top"></div>
+				<div class="bottom"></div>
+				<div class="left"></div>
+				<div class="right"></div>
+			</div>
+			<div class="cube-20 w-100 cube">
+				<div class="front">
+					<label for="message" class="label">Mensaje</label>
+				</div>
+				<div class="back"></div>
+				<div class="top"></div>
+				<div class="bottom"></div>
+				<div class="left"></div>
+				<div class="right"></div>
+			</div>
+			<div class="cube-21 w-300 cube">
+				<div class="front"></div>
+				<div class="back"></div>
+				<div class="top"></div>
+				<div class="bottom"></div>
+				<div class="left"></div>
+				<div class="right"></div>
+			</div>
+			<div class="cube-22 w-300 cube">
+				<div class="front">
+					<textarea name="message" id="message" placeholder="Mensaje..." class="field textarea"></textarea>
+				</div>
+				<div class="back"></div>
+				<div class="top"></div>
+				<div class="bottom"></div>
+				<div class="left"></div>
+				<div class="right"></div>
+			</div>
+			<div class="cube-23 w-100 cube">
+				<div class="front"></div>
+				<div class="back"></div>
+				<div class="top"></div>
+				<div class="bottom"></div>
+				<div class="left"></div>
+				<div class="right"></div>
+			</div>
+			<div class="cube-24 w-100 cube">
+				<div class="front"></div>
+				<div class="back"></div>
+				<div class="top"></div>
+				<div class="bottom"></div>
+				<div class="left"></div>
+				<div class="right"></div>
+			</div>
+			<div class="cube-25 w-100 cube">
+				<div class="front"></div>
+				<div class="back"></div>
+				<div class="top"></div>
+				<div class="bottom"></div>
+				<div class="left"></div>
+				<div class="right"></div>
+			</div>
+			<div class="cube-26 w-300 cube">
+				<div class="front"></div>
+				<div class="back"></div>
+				<div class="top"></div>
+				<div class="bottom"></div>
+				<div class="left"></div>
+				<div class="right"></div>
+			</div>
+			<div class="cube-27 w-100 cube">
+				<div class="front"></div>
+				<div class="back"></div>
+				<div class="top"></div>
+				<div class="bottom"></div>
+				<div class="left"></div>
+				<div class="right"></div>
+			</div>
+			<div class="cube-28 w-100 cube">
+				<div class="front">
+				</div>
+				<div class="back"></div>
+				<div class="top"></div>
+				<div class="bottom"></div>
+				<div class="left"></div>
+				<div class="right"></div>
+			</div>
+			<div class="cube-29 w-300 cube">
+				<div class="front"></div>
+				<div class="back"></div>
+				<div class="top"></div>
+				<div class="bottom"></div>
+				<div class="left"></div>
+				<div class="right"></div>
+			</div>
+			<div class="cube-30 w-300 cube">
+				<div class="front"></div>
+				<div class="back"></div>
+				<div class="top"></div>
+				<div class="bottom"></div>
+				<div class="left"></div>
+				<div class="right"></div>
+			</div>
+			<div class="cube-31 w-300 cube">
+				<div class="front">
+					<input type="text" name="gossip" id="gossip" placeholder="¿Cómo supiste de nosotros?"
+						class="field" />
+				</div>
+				<div class="back"></div>
+				<div class="top"></div>
+				<div class="bottom"></div>
+				<div class="left"></div>
+				<div class="right"></div>
+			</div>
+			<div class="cube-32 w-300 cube">
+				<div class="front"></div>
+				<div class="back"></div>
+				<div class="top"></div>
+				<div class="bottom"></div>
+				<div class="left"></div>
+				<div class="right"></div>
+			</div>
+			<div class="cube-33 w-300 cube">
+				<div class="front">
+					<button id="contact-stack-button" type="button" class="button">
+						Enviar
+					</button>
+				</div>
+				<div class="back"></div>
+				<div class="top"></div>
+				<div class="bottom"></div>
+				<div class="left"></div>
+				<div class="right"></div>
+			</div>
+		</form>
+    )
+}
+
+export default Contact;
