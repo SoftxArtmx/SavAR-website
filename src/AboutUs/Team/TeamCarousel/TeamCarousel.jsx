7289c9ab64755256d133872fb254db5a8ef16fcc
Marcos Juarez <>
2024-11-08 20:17:57 -0600

    Update: Profile pictures

--- src/AboutUs/Team/TeamCarousel/TeamCarousel.jsx
+++ src/AboutUs/Team/TeamCarousel/TeamCarousel.jsx
@@ -10,12 +10,13 @@ import pfpCM from '../../../assets/pfps/pfpCarlosM.jpeg'
 import pfpLZ from '../../../assets/pfps/pfpLeo.jpeg'
 import pfpGufi from '../../../assets/pfps/pfpGufi.jpeg'
 import pfpDM  from '../../../assets/pfps/pfpDiego.jpeg'
+import pfpHS from '../../../assets/pfps/pfpHer.jpeg'
 
 function TeamCarousel() {
   const autoplay = useRef(Autoplay({ delay: 3000 }));
   const isLargeScreen = useMediaQuery('(min-width: 1280px)');
-  let gitLinks = { "CARLOS":"https://github.com/carlosmedina8", "HER":"", "REDE":"https://github.com/dgmnzrd", "MARCOS":"https://github.com/marcosjn19", "GAEL":"https://github.com/GaelCG" , "RODRI":"", "LEONARDO": "https://github.com/leozavglz" }
-  let igLinks = { "CARLOS":"https://www.instagram.com/wave.kiddd/", "HER":"", "REDE":"https://www.instagram.com/rede.js/", "MARCOS":"https://www.instagram.com/marcos_jn19/", "GAEL":"https://www.instagram.com/gael.cosg/" , "RODRI":"", "LEONARDO": "https://www.instagram.com/leozglz/" }
+  let gitLinks = { "CARLOS":"https://github.com/carlosmedina8", "HER":"https://github.com/shaniValkyrie", "REDE":"https://github.com/dgmnzrd", "MARCOS":"https://github.com/marcosjn19", "GAEL":"https://github.com/GaelCG" , "RODRI":"", "LEONARDO": "https://github.com/leozavglz" }
+  let igLinks = { "CARLOS":"https://www.instagram.com/wave.kiddd/", "HER":"https://www.instagram.com/herstttt/", "REDE":"https://www.instagram.com/rede.js/", "MARCOS":"https://www.instagram.com/marcos_jn19/", "GAEL":"https://www.instagram.com/gael.cosg/" , "RODRI":"", "LEONARDO": "https://www.instagram.com/leozglz/" }
   let lkdnLinks = { "CARLOS":"https://github.com/carlosmedina8", "HER":"", "REDE":"https://www.instagram.com/rede.js/", "MARCOS":"https://www.linkedin.com/in/marcos-emmanuel-ju%C3%A1rez-navarro-51a3b9322/", "GAEL":"https://github.com/GaelCG" , "RODRI":"", "LEONARDO": "https://www.linkedin.com/in/leonardo-zavala-gonz%C3%A1lez-53975b180/" }
   return (
     <>
@@ -32,7 +33,7 @@ function TeamCarousel() {
       dragFree = "true"
       loop
       >
-        <Carousel.Slide><Member puesto="CEO" title="Hermione Sandoval" imgSrc={imagenFun1} description="'Saquen Valo'" linkGit={gitLinks["HER"]} linkIg = {igLinks["HER"]} linkLkdn = {lkdnLinks["HER"]} /></Carousel.Slide>
+        <Carousel.Slide><Member puesto="CEO" title="Hermione Sandoval" imgSrc={pfpHS} description="'Saquen Valo'" linkGit={gitLinks["HER"]} linkIg = {igLinks["HER"]} linkLkdn = {lkdnLinks["HER"]} /></Carousel.Slide>
         <Carousel.Slide><Member puesto="Vision Specialist" title="Leonardo Zavala" imgSrc={pfpLZ} description="'A un hombre no le preguntas dos cosas...
 Cuánto gana, ni tampoco cuánto lleva del sprint'" linkGit={gitLinks["LEONARDO"]} linkIg = {igLinks["LEONARDO"]} linkLkdn = {lkdnLinks["LEONARDO"]}/></Carousel.Slide>
         <Carousel.Slide><Member puesto="Dev" title="Carlos Medina" imgSrc={pfpCM} description="'El miedo lleva a la ira, la ira lleva al odio, el odio lleva al sufrimiento.'" linkGit={gitLinks["CARLOS"]} linkIg = {igLinks["CARLOS"]} linkLkdn = {lkdnLinks["CARLOS"]}/></Carousel.Slide>
